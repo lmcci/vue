401aca6e3f83dc396d38aa7f9dd8a26ae8e10ff8
lmcci <674645059@qq.com>
2019-08-08 17:47:42 +0800

    util

--- src/server/util.js
+++ src/server/util.js
@@ -1,9 +1,12 @@
 /* @flow */
 
+// 通过文件名判断是不是js文件
 export const isJS = (file: string): boolean => /\.js(\?[^.]+)?$/.test(file)
 
+// 通过文件名判断是不是css文件
 export const isCSS = (file: string): boolean => /\.css(\?[^.]+)?$/.test(file)
 
+// 返回一个对象有promise和cb 当cb被调用的时候 会执行promise的reject或者resolve
 export function createPromiseCallback () {
   let resolve, reject
   const promise: Promise<string> = new Promise((_resolve, _reject) => {
